cbd92ecbac46a6225a4e65704fbf623284007061
cgauci87 <cgauci@gmail.com>
2022-06-23 20:27:47 +0000

    rereplace run.py

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
-
 def is_valid_input(input_string, input_type):
     """
     this method is used to check if the input is
     valid Either it is Y/N (for yes/no)
     OR SMP (for single/married/parient)
-     """
+    """
     if input_type == "YN":
         if input_string == "Y" or input_string == "N":
             return True
@@ -53,22 +52,24 @@ class TaxCalculator:
         """
         This method is used to display the data for the user
         """
-        print(
-            f"\nGross Salary Breakdown for {user_name} is:")
-        print(
-            f"\t\t\t\t\tWeekly\t\t\t\tMonthly\t\t\t\tYearly")
+        print(f"\nGross Salary Breakdown for {user_name} is:")
+        print(f"\t\t\t\t\tWeekly\t\t\t\tMonthly\t\t\t\tYearly")
         print(
             f"Gross Salary: \t\t€{round(self.income/52,2)}\t\t\t\t€ \
-            {round(self.income/12,2)}\t\t\t\t€{self.income}")
+            {round(self.income/12,2)}\t\t\t\t€{self.income}"
+        )
         print(
             f"Tax: \t\t\t\t€{self.weekly_tax}\t\t\t\t€ \
-            {self.monthly_tax}\t\t\t\t€{self.yearly_tax}")
+            {self.monthly_tax}\t\t\t\t€{self.yearly_tax}"
+        )
         print(
             f"COLA/Bonus: \t\t€{self.cola_bonus_weekly}**\t\t\t\t€ \
-            {self.cola_bonus_monthly}**\t\t\t€{self.cola_bonus_yearly}**")
+            {self.cola_bonus_monthly}**\t\t\t€{self.cola_bonus_yearly}**"
+        )
         print(
             f"Net Salary: \t\t€{self.net_income_weekly}\t\t\t\t€ \
-            {self.net_income_monthly}\t\t\t€{self.net_income_yearly}")
+            {self.net_income_monthly}\t\t\t€{self.net_income_yearly}"
+        )
 
 
 class ParentTaxCalculator(TaxCalculator):
@@ -115,8 +116,8 @@ class ParentTaxCalculator(TaxCalculator):
     def calculate_net_income(self):
         """ calculate the net income for the user with civil status parent """
         self.net_income_yearly = self.income - self.yearly_tax
-        self.net_income_monthly = round(self.net_income_yearly/12, 2)
-        self.net_income_weekly = round(self.net_income_yearly/52, 2)
+        self.net_income_monthly = round(self.net_income_yearly / 12, 2)
+        self.net_income_weekly = round(self.net_income_yearly / 52, 2)
 
 
 class MarriedTaxCalculator(TaxCalculator):
@@ -164,8 +165,8 @@ class MarriedTaxCalculator(TaxCalculator):
     def calculate_net_income(self):
         """ calculate the net income for the user with civil status married """
         self.net_income_yearly = self.income - self.yearly_tax
-        self.net_income_monthly = round(self.net_income_yearly/12, 2)
-        self.net_income_weekly = round(self.net_income_yearly/52, 2)
+        self.net_income_monthly = round(self.net_income_yearly / 12, 2)
+        self.net_income_weekly = round(self.net_income_yearly / 52, 2)
 
 
 class SingleTaxCalculator(TaxCalculator):
@@ -213,8 +214,8 @@ class SingleTaxCalculator(TaxCalculator):
     def calculate_net_income(self):
         """ calculate the net income for the user with civil status single """
         self.net_income_yearly = self.income - self.yearly_tax
-        self.net_income_monthly = round(self.net_income_yearly/12, 2)
-        self.net_income_weekly = round(self.net_income_yearly/52, 2)
+        self.net_income_monthly = round(self.net_income_yearly / 12, 2)
+        self.net_income_weekly = round(self.net_income_yearly / 52, 2)
 
     # welcome message , description of the app
 
@@ -226,7 +227,8 @@ print(
     "\t\t\tPlease note that result may vary with your actual salary\n"
     "\t\t\tdue to other deductions such as national insurance,\n"
     "\t\t\tand/or other benefits i.e. allowances and bonuses.\n"
-    "\n\t\t\t**************************************************************\n")
+    "\n\t\t\t**************************************************************\n"
+)
 
 while True:
     user_name = str(input("Enter your name: ")).capitalize()
@@ -240,8 +242,10 @@ while True:
     # ask the user for the student status
     is_student = str(input("Are you a student? (Y/N): ")).upper()
     if not is_valid_input(is_student, "YN"):
-        print("Invalid input! Please try again.. \
-        If you are a student - type Y , if not - type N")
+        print(
+            "Invalid input! Please try again.. \
+        If you are a student - type Y , if not - type N"
+        )
         continue
     break
 while True:
@@ -249,29 +253,33 @@ while True:
     # ask the user for the age over 18
     is_over_18_years = str(input("Are you over 18? (Y/N): ")).upper()
     if not is_valid_input(is_over_18_years, "YN"):
-        print("Invalid input! Please try again.. \
-           If you are over 18 years of age - type Y, if not - type N")
+        print(
+            "Invalid input! Please try again.. \
+           If you are over 18 years of age - type Y, if not - type N"
+        )
         continue
     break
 while True:
     # if the under 18, skip to ask for before 1962 option
     if is_over_18_years == "Y":
         # ask the user for the born before 1962
-        is_born_before_1962 = str(
-            input("Are you born before 1962? (Y/N): ")).upper()
+        is_born_before_1962 = str(input("Are you born before 1962? (Y/N): ")).upper()
         if not is_valid_input(is_born_before_1962, "YN"):
-            print("Invalid input! Please try again.. \
-           if you were born before 1962 - type Y , if not - type N")
+            print(
+                "Invalid input! Please try again.. \
+           if you were born before 1962 - type Y , if not - type N"
+            )
             continue
     break
 while True:
     # ask the user for the civil status
-    civil_status = str(
-        input("Are you single, married or parent? (S/M/P): ")).upper()
+    civil_status = str(input("Are you single, married or parent? (S/M/P): ")).upper()
     if not is_valid_input(civil_status, "SMP"):
-        print("Invalid input! Please try again.. \
+        print(
+            "Invalid input! Please try again.. \
         if your status is Single - type S , \
-        if Married - type M , if Parent - type P")
+        if Married - type M , if Parent - type P"
+        )
         continue
     break
 while True:
@@ -281,7 +289,8 @@ while True:
     except ValueError:
         print(
             "Invalid input! Please try again by entering your income.\
-             Do not include special characters")
+             Do not include special characters"
+        )
         continue
     break
 while True:
@@ -314,11 +323,10 @@ while True:
 
         # Want to continue?
         continue_input = str(
-            input(
-                "Would you like to calculate other salary?(Y/N):")).upper()
+            input("Would you like to calculate other salary?(Y/N):")
+        ).upper()
         if not is_valid_input(continue_input, "YN"):
-            print(
-                "Invalid input! Please try again, by either type Y or N.")
+            print("Invalid input! Please try again, by either type Y or N.")
             continue
 
         if continue_input == "N":
