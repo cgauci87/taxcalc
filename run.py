f2d77049f04fbb8a9df215275f82bf7b73c1e3d0
cgauci87 <cgauci@gmail.com>
2022-06-24 06:12:36 +0000

    bug fix pep8 errors

--- run.py
+++ run.py
@@ -136,8 +136,8 @@ class ParentTaxCalculator(TaxCalculator):
         """
         print(f"\nGross Salary Breakdown for {user_name} is:")
         data = [
-            ['self.net_income_weekly, self.net_income_monthly, '
-             'self.net_income_yearly']
+            ('self.net_income_weekly, self.net_income_monthly, '
+             'self.net_income_yearly')
         ]
         print(
             tabulate(
@@ -205,8 +205,8 @@ class MarriedTaxCalculator(TaxCalculator):
         """
         print(f"\nGross Salary Breakdown for {user_name} is:")
         data = [
-            ['self.net_income_weekly, self.net_income_monthly, '
-             'self.net_income_yearly']
+            ('self.net_income_weekly, self.net_income_monthly, '
+             'self.net_income_yearly')
         ]
         print(
             tabulate(
@@ -274,8 +274,8 @@ class SingleTaxCalculator(TaxCalculator):
         """
         print(f"\nGross Salary Breakdown for {user_name} is:")
         data = [
-            ['self.net_income_weekly, self.net_income_monthly, '
-             'self.net_income_yearly']
+            ('self.net_income_weekly, self.net_income_monthly, '
+             'self.net_income_yearly')
         ]
         print(
             tabulate(
