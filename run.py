4e11fa03616e3ec0355557e7bc45216e6140c1c5
cgauci87 <cgauci@gmail.com>
2022-06-30 18:13:19 +0000

    add google auth and gspread in requirements.txt

--- run.py
+++ run.py
@@ -1,5 +1,25 @@
 from tabulate import tabulate
 
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('taxcalc_user_data')
+
+salaries = SHEET.worksheet('salaries')
+
+data = salaries.get_all_values()
+
+print(data)
+
 
 def is_valid_input(input_string, input_type):
     """
